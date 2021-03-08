c31184558a6755dbf3f7cf1684ab146755d65d26
Gustavo Pacheco <ryctabo@gmail.com>
2021-03-08 00:19:33 -0500

    chore: Initial commit

---
+++ src/main/java/com/ryctabo/spring/app/SpringRedisDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.ryctabo.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisDemoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryctabo/spring/app/SpringRedisDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryctabo.spring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
